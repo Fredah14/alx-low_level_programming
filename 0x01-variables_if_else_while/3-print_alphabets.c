f413846393282566d661768e0246a1f64b98619c
Fredah14 <fredah14>
2023-02-18 10:08:18 -0800

    3. alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,12 +5,10 @@
 */
 int main(void)
 {
-  char alp[26] = "abcdefghijklmnopqrstuvwxyz" "ABCDEFGHIJKLMOPQRSTUVWXYZ";
-int i;
-for (i =0; i < 26; i++)
-{
-putchar(alp[i]);
-}
-putchar('\n');
+int ch;
+for (ch = 'a'; ch = 'z;; ch++)
+putchar(ch);
+for (ch = 'A'; ch <= 'Z'; ch++)
+putchar(ch);
 return (0);
 }
