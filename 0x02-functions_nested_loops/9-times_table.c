05d834b669dc1b10a415513f0bdd774d4dddf1a5
Fredah14 <fredah14>
2023-02-22 12:13:10 -0800

    9. Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,9 +3,7 @@
 /**
 *times_table - Prints the 9 times table, starting with 0.
 *
-*Return: the 9 times table starting with 0
 */
-
 
 void times_table(void)
 {
@@ -25,8 +23,7 @@ _putchar(',');
 _putchar(' ';
 _putchar(' ';
 _putchar(k + '0');
-}
-else if (k >= 10)
+} else if (k >= 10)
 {
 _putchar(',');
 _putchar(' ');
