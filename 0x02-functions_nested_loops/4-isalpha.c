63b404bc9d3e59b43d442c2a2dd6ffd5d0370eb8
Fredah14 <fredah14>
2023-02-22 09:44:01 -0800

    4. isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+*_isalpha - Checks for the alphabetic character
+*
+*Return: 1 if c is a letter lowercase or uppercase, otherwise return 0
+*/
+
+int _isalpha(int c);
+{
+return ((c >= 'a' && <= 'z') || (c >= 'A' && c <= 'Z'));
+}
