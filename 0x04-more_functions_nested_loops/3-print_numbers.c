14929abe379b3d5a275918a23dff97e6582b7d8b
Fredah14 <fredah14>
2023-02-26 09:54:54 -0800

    The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,13 @@
 #include "main.h"
-
 /**
-* print_numbers - Prints the numbers from 0 to 9
+* print_numbers - prints numbers from 0 to 9
 *
-* Return: void*/
+* Return: Always 0.
+*/
 void print_numbers(void)
 {
-char c;
-for (c = '0'; c <= '9'; c++)
+int c;
+for (c = 48; c < 58; c++)
 {
 _putchar(c);
 }
