561c4c319023b56e80be9fdfed95215dc9bf034d
Fredah14 <fredah14>
2023-03-07 02:41:18 -0800

    2. strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,8 +3,8 @@
  * _strchr - Entry point
  * @s: input
  * @c: input
- * Return: A pointer to the first occurrence of the character c 
- *in the string s, or NULL if the character is not found
+ * Return: A pointer to the first occurrence of the character c
+ * in the string s, or NULL if the character is not found
  */
 char *_strchr(char *s, char c)
 {
