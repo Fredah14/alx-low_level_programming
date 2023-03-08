f8557f0b234d0c7e5a3370bc67c9b444b762cbb5
Fredah14 <fredah14>
2023-03-08 09:21:52 -0800

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,10 +2,18 @@
 
 /**
  *_puts_recursion - Function that prints a string, followed by a new line.
+ *@s: Input
  *
- ***
  *Return: Always 0.
  */
 
 void _puts_recursion(char *s)
 {
+if (*s)
+{
+_putchar(*s);
+_puts_recursion(s + 1);
+}
+else
+_putchar('\n');
+}
