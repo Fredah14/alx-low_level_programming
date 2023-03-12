75648d646a4ed26c23518e8db224db50207fc489
Fredah14 <fredah14>
2023-03-12 07:01:14 -0700

    0. It ain't what they call you, it's what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Prints its name, followed by a new line.
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: Always 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+printf("%s\n", *argv);
+return (0);
+}
