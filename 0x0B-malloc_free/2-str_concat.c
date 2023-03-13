5ee02285c37280692451565a58edf2c40d1e0116
Fredah14 <fredah14>
2023-03-13 12:48:10 -0700

    2. He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -4,10 +4,12 @@
 
 /**
  *str_concat - Concatenates two strings.
- *@s1: str
- *@s2: str
- *Return: Always 0.
+ *@s1: First str
+ *@s2: Second str
+ *Return: Pointer to the new str or NULL on failure
  */
 
 char *str_concat(char *s1, char *s2)
 {
+  char *s3;
+  unsigned int = 0, j = 0, len1 = 0, len2 = 0;
