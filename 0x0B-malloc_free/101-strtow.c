7b390b41ecc179ea2b705ada8c8df8e32110b43f
Fredah14 <fredah14>
2023-03-15 12:56:54 -0700

    6. I will show you how great I am

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *strtow - Splits a string into words.
+ *@s: String to evaluate
+ *Return: A pointer to an array of strings (words),
+ *If it fails, it should return NULL
+ */
+
+char **strtow(char *str)
+{
