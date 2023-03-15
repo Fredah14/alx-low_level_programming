5fa024c2538e149f71f3445156a24afa3a20e947
Fredah14 <fredah14>
2023-03-15 12:37:04 -0700

    5. It isn't the mountains ahead to climb that wear you out; it's the pebble in your shoe

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -11,32 +11,36 @@
 
 char *argstostr(int ac, char **av)
 {
-int i, j, k = 1, len = 0;
-char *str;
+	int i, j, k, len;
+	char *str;
 
-if (ac == 0 || av == NULL)
-return (NULL);
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+			len++;
+		len++;
+	}
+
+	str = malloc(sizeof(char) * (len + 1));
+
+	if (str == NULL)
+		return (NULL);
+
+	k = 0;
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			str[k] = av[i][j];
+			k++;
+		}
+		str[k] = '\n';
+		k++;
+	}
 	
-for (i = 0; i < ac; i++)
-{
-for (j = 0; av[i][j]; j++)
-len++;
-}
-len += ac;
-str = malloc(sizeof(char) * len + 1);
-if (str == NULL)
-return (NULL);
-for (i = 0; i < ac; i++)
-{
-for (j = 0; av[i][j]; j++)
-{
-str[k] = av[i][j];
-k++;
-}
-if (str[k] == '\0')
-{
-str[k++] = '\n';
-}
-}
-return (str);
+	return (str);
 }
