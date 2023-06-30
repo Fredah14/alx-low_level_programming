d8ac9c47bc37d39e924ee5ef1c7cf0cc19c3c853
Fredah14 <fredah.jr@hotmail.com>
2023-06-30 15:00:48 -0700

    0. Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,7 +14,7 @@ size_t nodes = 0;
 while (h != NULL)
 {
 if (h->str == NULL)
-printf("[%d] %s\n", 0, "(nil)";
+printf("[%d] %s\n", 0, "(nil)");
 else
 printf("[%d] %s\n", h->len, h->str);
 h = h->next;
