a92488189876be3cc1877e6ba913500fc660d47e
Fredah14 <fredah.jr@hotmail.com>
2023-06-30 15:41:42 -0700

    1. List length

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "lists.h"
 
 /**
@@ -8,12 +9,12 @@
  */
 size_t list_len(const list_t *h)
 {
-size_t nodes = 0;
-
+size_t nodes;
+nodes = 0;
 while (h != NULL)
 {
-nodes++;
 h = h->next;
+nodes++;
 }
 return (nodes);
 }
