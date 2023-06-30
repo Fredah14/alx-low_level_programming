200539372f279ffa78c49ecb9141f5bb9c100064
Fredah14 <fredah.jr@hotmail.com>
2023-06-30 15:06:41 -0700

    0. Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,12 +9,13 @@
  */
 size_t print_list(const list_t *h)
 {
-size_t nodes = 0;
+size_t nodes;
+nodes = 0;
 
 while (h)
 {
 if (!h->str)
-printf("[%d] %s\n", 0, "(nil)");
+printf("[0](nil)\n");
 else
 printf("[%d] %s\n", h->len, h->str);
 h = h->next;
