3b7931c3fbddfbb45b7bc84874a5aa2f211bbd22
Fredah14 <fredah.jr@hotmail.com>
2023-06-30 17:27:22 -0700

    5. The Hare and the Tortoise

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,12 +3,12 @@
 
 /**
  * print_bef - prints a sentence before the main function is executed.
- * function is executed
+ *
  * Return: Always 0.
  */
 
-void print_bef(void) __attribute__ ((constructor));
-void print_bef()
+void print_bef(void) __attribute__ ((constructor))
+void print_bef(void)
 {
 printf("You're beat! and yet, you must allow");
 printf(",\nI bore my house upon my back!\n");
