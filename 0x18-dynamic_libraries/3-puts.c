e2447a6ac75811b9cf85ac986e8c789192060983
Fredah14 <fredah.jr@hotmail.com>
2023-08-22 15:24:17 -0700

    0. A library is not a luxury but one of the necessities of life

---
+++ 0x18-dynamic_libraries/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*_puts - Prints a string, followed by a new line, to stdout.
+*@str: the str
+*Return: Always 0.
+*/
+
+void _puts(char *str)
+{
+int a = 0;
+while (str[a] != '\0')
+{
+_putchar(str[a]);
+a++;
+}
+_putchar('\n');
+}
